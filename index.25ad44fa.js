48cee93f6b0b34c460c6b242b11dbabf3af21769
sergio-nezhigay <58699786+sergio-nezhigay@users.noreply.github.com>
2023-01-28 20:32:23 +0000

    Deploying to gh-pages from @ sergio-nezhigay/legal@d02c7283531f0d14498987035df8a04de149b3ef 🚀

---
+++ index.25ad44fa.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(e,n){return'<span class="'+n+'">'+(e+1)+"</span>"}}});
+//# sourceMappingURL=index.25ad44fa.js.map
