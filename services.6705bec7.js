fa2a552c8e70d948408e35a3d6dec7e21a867b89
sergio-nezhigay <58699786+sergio-nezhigay@users.noreply.github.com>
2023-02-10 19:43:16 +0000

    Deploying to gh-pages from @ sergio-nezhigay/legal@39b53f7236d1e5c1b179c990d34e6d02697c1477 🚀

--- services.6705bec7.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){console.log("toggleModal ..."),o.modal.classList.toggle("is-hidden")}console.log("modal started..."),o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=services.6705bec7.js.map
